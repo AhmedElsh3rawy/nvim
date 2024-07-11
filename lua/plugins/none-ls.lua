7582510fbe8c150a3174f945c353f3e985e0c064
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-11 14:26:18 +0300

    goimports

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,7 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.goimports,
         -- null_ls.builtins.formatting.golines,
         null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.mypy,
