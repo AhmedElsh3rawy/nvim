d219f4a6477426618137c176a9c8ae72d0be0b2d
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-16 14:27:01 +0300

    add some remaps and optoins

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,11 @@
 local builtin = require("telescope.builtin")
 local K = vim.keymap
 K.set("n", "<leader>ff", builtin.find_files, { desc = "Find file" })
-K.set("n", "<leader>fw", builtin.live_grep, {})
-K.set("n", "<leader>fb", builtin.buffers, {})
-K.set("n", "<leader>fh", builtin.help_tags, {})
-K.set("n", "<leader>e", ":NvimTreeToggle<CR>", {})
+K.set("n", "<leader>fw", builtin.live_grep, { desc = "Find word" })
+K.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
+K.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
+K.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Nvim tree" })
+K.set("n", "<leader>bc", ":BufferLinePickClose<CR>", { desc = "Pick buffer to close" })
 K.set("i", "jn", "<Esc>", {})
 -- Make neovim transparant
 K.set("n", "<leader>T", ":lua TransColor()<CR>", {})
--- lua/options.lua
+++ lua/options.lua
@@ -1,17 +1,19 @@
+local opt = vim.opt
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.wo.relativenumber = true
-vim.opt.number = true
-vim.opt.swapfile = false
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.tabstop = 2 -- Number of spaces a tab counts for
-vim.opt.shiftwidth = 2 -- Number of spaces used for each step of (auto)indent
-vim.opt.softtabstop = 2 -- Number of spaces a tab displays as
-vim.opt.fillchars = { eob = " " } -- Remove the ~ line
-vim.opt.undofile = true
+opt.number = true
+opt.swapfile = false
+opt.expandtab = true          -- Use spaces instead of tabs
+opt.tabstop = 2               -- Number of spaces a tab counts for
+opt.shiftwidth = 2            -- Number of spaces used for each step of (auto)indent
+opt.softtabstop = 2           -- Number of spaces a tab displays as
+opt.fillchars = { eob = " " } -- Remove the ~ line
+opt.undofile = true
 -- Copy to system clipboard
-vim.opt.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
 vim.o.signcolumn = "no" -- Remove the W char beside the line number
 vim.o.background = "dark"
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
+opt.termguicolors = true
+opt.scrolloff = 8
