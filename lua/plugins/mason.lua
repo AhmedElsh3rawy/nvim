bc72e25c48035240cd9691d856ff204fd9062587
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-16 14:26:27 +0300

    show dotfiles in nvim-tree

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,25 +1,14 @@
 return {
-  {
-  "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-      --    ensure_installed = {
-      --   "rust_analyzer",
-      --   "tsserver",
-      --   "biome",
-      --   "clangd",
-      --   "gopls",
-      --   "pyright",
-      --   "ruff",
-      --   "lua_ls",
-      -- },
-      })
-    end
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({})
+		end,
+	},
 }
